29c417bd8e0ff52005b6c2df9b2a840d8a8ab626
wangtingting8 <wangtt12034@126.com>
2016-09-01 15:48:29 +0800

    webpack less node_modules

---
+++ demo/entry.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wangtingting8 on 2016/8/31.
+ */
+document.write('It works!');
+document.write(require('../module.js'));//添加模块
+require('../style.css');
---
+++ module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wangtingting8 on 2016/8/31.
+ */
+module.exports = 'Its module.js';
